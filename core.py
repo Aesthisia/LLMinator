bc39d76c48dd792eb7d00f49defeb5132841a864
Ashish <ashish@aesthisia.com>
2024-03-31 22:11:55 +0530

    List & remove cached models + Updated blocks

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 
 def format_model_name(directory_name):
     parts = directory_name.split("--")
@@ -6,7 +6,12 @@ def format_model_name(directory_name):
 
 def list_download_models(cache_dir):
     contents = os.listdir(cache_dir)
-    directories = [item for item in contents if os.path.isdir(os.path.join(cache_dir, item)) and item.startswith("models")]
+    directories = [format_model_name(item) for item in contents if os.path.isdir(os.path.join(cache_dir, item)) and item.startswith("models")]
     return directories
 
-
+def remove_dir(path):
+    try:
+        shutil.rmtree(os.path.join(path, "/*"))
+        print(f"Directory '{path}' successfully removed.")
+    except OSError as e:
+        print(f"Error: {e.strerror}")
