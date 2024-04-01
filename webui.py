9271d74bdb3d5ca3ddca72f3b4b7fe0653bfeafa
Subhanshu0027 <91900622+Subhanshu0027@users.noreply.github.com>
2024-04-01 17:10:18 +0530

    Added config.ini

--- webui.py
+++ webui.py
@@ -1,19 +1,28 @@
 import os, torch
 from threading import Thread
 from typing import Optional
+import argparse
 
+from configparser import ConfigParser
 import gradio as gr
 from langchain import PromptTemplate, LLMChain
 from langchain.llms.base import LLM
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer, AutoConfig
 
-from core import list_download_models, format_model_name, remove_dir
+from core import list_download_models, remove_dir
+
+def read_config():
+    config = ConfigParser()
+    config.read('config.ini')
+    return config
+
+def update_config(config):
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
 
 cache_dir = os.path.join(os.getcwd(), "models")
 saved_models = list_download_models(cache_dir)
-
-#check if cuda is available
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def initialize_model_and_tokenizer(model_name):
     config = AutoConfig.from_pretrained(model_name, cache_dir=cache_dir)
@@ -26,8 +35,7 @@ def initialize_model_and_tokenizer(model_name):
         trust_remote_code=True)
     
     model.eval()
-    model.to(device)
-
+    model.cpu()
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return model, tokenizer
@@ -59,7 +67,49 @@ def init_chain(model, tokenizer):
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     return llm_chain, llm
 
-model, tokenizer = initialize_model_and_tokenizer("openai-community/gpt2")
+def parse_args():
+    parser = argparse.ArgumentParser(description='Optional arguments for --host & --port.') 
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='The host IP to run the server on.')
+    parser.add_argument('--port', type=int, default=7860, help='The port to run the server on.')
+
+    return parser.parse_args()
+
+args = parse_args()
+
+config = read_config()  # <--- CHANGE HERE
+cache_dir = os.path.join(os.getcwd(), "models")
+saved_models = list_download_models(cache_dir)
+
+model_id = config.get('Settings', 'repo_id')
+model, tokenizer = initialize_model_and_tokenizer(model_id)
+
+def update_execution_provider(provider):
+    config = read_config()
+    config.set('Settings', 'execution_provider', provider)
+    update_config(config)
+    if provider == "cuda":
+        if torch.cuda.is_available():
+            model.cuda()
+            print("Model loaded in cuda", model)
+        else:
+            raise gr.Error("Torch not compiled with CUDA enabled. Please make sure cuda is installed.")
+    else:
+        model.cpu()
+
+def load_model(repo_id):
+    global llm_chain, llm
+    if repo_id:
+        model, tokenizer = initialize_model_and_tokenizer(repo_id)
+        llm_chain, llm = init_chain(model, tokenizer)
+        # Read the current configuration
+        config = read_config()
+        # Update the repo_id in the configuration
+        config.set('Settings', 'repo_id', repo_id)
+        # Write the updated configuration back to the config.ini file
+        update_config(config)
+        return gr.update(value=repo_id)
+    else:
+        raise gr.Error("Repo can not be empty!")
 
 with gr.Blocks(fill_height=True) as demo:
     with gr.Row():
@@ -71,7 +121,7 @@ with gr.Blocks(fill_height=True) as demo:
                 interactive=True)
             with gr.Group():
                 repo_id = gr.Textbox(
-                    value="openai-community/gpt2",
+                    value=model_id,
                     label="Hugging Face Repo",
                     info="Default: openai-community/gpt2")
                 load_model_btn = gr.Button(
@@ -82,7 +132,7 @@ with gr.Blocks(fill_height=True) as demo:
             with gr.Group():
                 execution_provider = gr.Radio(
                     ["cuda", "cpu"], 
-                    value=device, 
+                    value="cpu", 
                     label="Execution providers",
                     info="Select Device")
 
@@ -117,7 +167,6 @@ with gr.Blocks(fill_height=True) as demo:
         if provider == "cuda":
             if torch.cuda.is_available():
                 model.cuda()
-                print("Model loaded in cuda", model)
             else:
                 raise gr.Error("Torch not compiled with CUDA enabled. Please make sure cuda is installed.")
 
@@ -138,16 +187,17 @@ with gr.Blocks(fill_height=True) as demo:
         llm_chain.run(question=history[-1][0])
         history[-1][1] = ""
         for character in llm.streamer:
-            print(character)
+            #print(character)
             history[-1][1] += character
             yield history
 
     submit_event = msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
     stop.click(None, None, None, cancels=[submit_event], queue=False)
     load_model_btn.click(loadModel, repo_id, repo_id, queue=False, show_progress="full")
-    execution_provider.change(fn=updateExecutionProvider, inputs=execution_provider, queue=False, show_progress="full")
-    saved_models.change(loadModel, saved_models, repo_id, queue=False, show_progress="full")
+    execution_provider.change(fn=update_execution_provider, inputs=execution_provider, queue=False, show_progress="full")
+    saved_models.change(load_model, saved_models, repo_id, queue=False, show_progress="full")
     offload_models.click(removeModelCache, None, saved_models, queue=False, show_progress="full")
 
+
 demo.queue()
-demo.launch(server_name="0.0.0.0")
+demo.launch(server_name = args.host , server_port = args.port)
